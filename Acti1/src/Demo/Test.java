8c3b6fb302d7906ee345442134267d698fbb197c
Nishikant <Nishikant@LAPTOP-QO0BQ598>
2020-10-11 17:10:21 +0530

    Ecl

--- Acti1/src/Demo/Test.java
+++ Acti1/src/Demo/Test.java
@@ -3,7 +3,7 @@ package Demo;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHUb");
+		System.out.println("Hello Eclipse");
 		// TODO Auto-generated method stub
 
 	}
